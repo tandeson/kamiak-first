4940d08d56251295b19bc7ec06e809c8b24f7dda
tandeson <tandeson@d60288e0-ce87-11de-83be-cfe0592a616e>
2009-11-16 07:00:55 +0000

    Updated Example two to include a function.

--- trunk/learningCpp/trunk/Example2/main.cpp
+++ trunk/learningCpp/trunk/Example2/main.cpp
@@ -1,5 +1,5 @@
 /***************************************************************
-Title: Example 1
+Title: Example 2
 Origional Author: Thomas Anderson
 Most Recent Author: $Author$
 Creation Date: November 11th 2009
@@ -29,6 +29,15 @@ using namespace std;
 // * Functions *
 // *************
 
+// add() function
+//
+// This function adds two numbers, and returns the results.
+//
+// The declaration bellow tells the computer the somewhere bellow we will define
+//  a function that takes in two integers (called x and y), and returns the
+//  the results as an integer.
+int add(int x, int y);
+
 // Main Function
 //
 // The main function is a special case. It's the function the computer always
@@ -50,9 +59,40 @@ int main()
 	// cout is a function which will dump the text to the standard 
 	// output, in this case the screen.
 	cout << "Hello World\n";
+    
+    // Declare two variables. Both are type int. numberBefore is 
+    //  initialized to 5, and numberAfter is not initialized.
+    int numberBefore = 5;
+    int numberAfter;
 
+    // Show the user the value of our two numbers
+    cout << " Before we add, numberBefore is " << numberBefore;
+    cout << " and numberAfter is " << numberAfter << endl;
+    
+    // call our function, passing in the value of numberBefore for both terms.
+    numberAfter = add(numberBefore,numberBefore);
+    
+    // show the user the values of our two numbers
+    cout << " After  we add, numberBefore is " << numberBefore;
+    cout << " and numberAfter is " << numberAfter << endl;
+    
 	// It's good practice to return a 0 from main. In most cases it
 	// doesn't matter, but sometimes other program will look at this to
 	// see if main quit the way it was intened to. 
 	return 0;
+}
+
+
+
+// Implimentatoin of add()
+// 
+// This is where we tell the computer what to do when the add function is called
+//
+// The function header must match our declartion down here, or the code will
+//  not build. The compiler even pays attention to capitalzation. so if this was
+//  Add() instead of add() it would give us an error.
+int add(int x, int y)
+{
+    // take x and y, add them together and return the result
+    return x + y;
 }
