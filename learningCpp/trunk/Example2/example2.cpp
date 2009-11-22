270257a3b7352538cd38a76a9231ac8531c51c77
tandeson <tandeson@d60288e0-ce87-11de-83be-cfe0592a616e>
2009-11-22 04:23:55 +0000

    Added a function showing an overflow, removed some comments which were in the first example.

--- learningCpp/trunk/Example2/example2.cpp
+++ learningCpp/trunk/Example2/example2.cpp
@@ -1,6 +1,6 @@
 /***************************************************************
 Title: Example 2
-Origional Author: Thomas Anderson
+Original Author: Thomas Anderson
 Most Recent Author: $Author$
 Creation Date: November 11th 2009
 Last Update Date: $Date$
@@ -12,22 +12,18 @@ SVN Location: $HeadURL$
 //* Include file(s)*
 //******************
 // This file pulls in code that allows us to send data to the screen so a
-//  user (like us!) can see it. We will be using the fucntion cout, which is 
+//  user (like us!) can see it. We will be using the function cout, which is 
 //  declared in the iostream header file.
 #include <iostream>
 
 //*************************
 //* Namespace declaration *
 //*************************
-
-// Don't worry about this for now, this just make our code easier to read.
-//  if we didn't have this, we'd need to type std::cout instead of just cout
-//  below in main().
 using namespace std;
 
-// *************
-// * Functions *
-// *************
+// ********************
+// * Function Headers *
+// ********************
 
 // add() function
 //
@@ -38,22 +34,21 @@ using namespace std;
 //  the results as an integer.
 int add(int x, int y);
 
-// Main Function
+// overflow() function
 //
-// The main function is a special case. It's the function the computer always
-//  runs first. It is always in the format
-//  int main()
-//     or
-//  int main(void)
-//  they both mean the same thing. If there is no main() function, the program
-//  won't compile.
+// This function prints out a 1 byte value, and then overflows it.
+void overflow();
+
+// *****************
+// * Function Code *
+// *****************
+
+//------------------------------------------------------------------------------
+// main() Function
 //
-// The int below tells the computer we are going to return an integer variable
-//  when the function is finished running (see the 'return 0;' call at the bottom)
-// main() tells the computer we have a function called main, which doesn't take
-//  any paramaters (also called arguments sometimes.)
+// Standard startup function.
 int main()
-{ // the { and } brakets tell the computer where main start and ends.
+{
 
 	// Take the text (also called a string) and pass it to cout.
 	// cout is a function which will dump the text to the standard 
@@ -76,23 +71,58 @@ int main()
     cout << " After  we add, numberBefore is " << numberBefore;
     cout << " and numberAfter is " << numberAfter << endl;
     
+    // Call our overflow() function to look at what what happens if we
+    //  exceed our variable's range.
+    overflow();
+    
 	// It's good practice to return a 0 from main. In most cases it
 	// doesn't matter, but sometimes other program will look at this to
-	// see if main quit the way it was intened to. 
+	// see if main quit the way it was intended to. 
 	return 0;
 }
 
-
-
-// Implimentatoin of add()
+//------------------------------------------------------------------------------
+// Implementation of add()
 // 
 // This is where we tell the computer what to do when the add function is called
 //
-// The function header must match our declartion down here, or the code will
-//  not build. The compiler even pays attention to capitalzation. so if this was
+// The function header must match our declaration down here, or the code will
+//  not build. The compiler even pays attention to capitalization. so if this was
 //  Add() instead of add() it would give us an error.
 int add(int x, int y)
 {
+    // A debuging note - this allows us to see when this function is called.
+    cout << " ** We called add(" << x << "," << y << ")! **\n";
+    
     // take x and y, add them together and return the result
     return x + y;
+}
+
+//------------------------------------------------------------------------------
+// Implementation of overflow()
+void overflow()
+{
+    // A debuging note - this allows us to see when this function is called.
+    cout << " ** We called overflow()! **\n";
+    
+    // declar a 1 byte variable, with a value of 100.
+    unsigned char ourLocalChar = 100; // unsigned char can hold from 0 to 255.
+    
+    // Display the current value of ourLocalChar
+    //
+    // The (int) is used to type cast from a char to a int. This is because
+    //  cout wants to show a char as a single character. 
+    //
+    // if you want to see this removed (int) from the line below, and instead of
+    // showing 100 your program output will show a "d". This is because 100 in
+    // ASCII represents a d. you can see a conversion table at 
+    // http://www.asciitable.com/ if your interested...
+    cout << " Our Local Char variable is " << (int)ourLocalChar << endl;
+    
+    // now, the max value of char is 255. What happens if I add 156 to
+    //  ourLocalChar which is at 100?
+    ourLocalChar = ourLocalChar + 157;
+    
+    // Display the current value of ourLocalChar
+    cout << " Our Local Char variable is " << (int)ourLocalChar << endl;
 }
