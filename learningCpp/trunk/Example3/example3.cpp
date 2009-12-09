8c727c636377731c9db943e2f12af804ae60befb
tandeson <tandeson@d60288e0-ce87-11de-83be-cfe0592a616e>
2009-12-09 02:31:30 +0000

    Changed out code to illustrate a structure.

--- learningCpp/trunk/Example3/example3.cpp
+++ learningCpp/trunk/Example3/example3.cpp
@@ -16,28 +16,39 @@ SVN Location: $HeadURL$
 //  declared in the iostream header file.
 #include <iostream>
 
+// This file pulls in code which allows easily handling of text strings with the 
+//  string class. (we'll cover classes later..)
+#include <string>
+
 //*************************
 //* Namespace declaration *
 //*************************
 using namespace std;
 
+
+// **************
+// * Structures *
+// **************
+
+// Here we are declairing a structure. In this case, it is a collection of varaibles.
+//  So we know that every student has both a name (hopefully!) and an age. You could add more
+//  I stopped at these two for simplicity's sake.
+struct student
+{
+	string Name;
+	int    age;
+};
+
+
 // ********************
 // * Function Headers *
 // ********************
 
-// add() function
+// ShowStudent
 //
-// This function adds two numbers, and returns the results.
-//
-// The declaration bellow tells the computer the somewhere bellow we will define
-//  a function that takes in two integers (called x and y), and returns the
-//  the results as an integer.
-int add(int x, int y);
-
-// overflow() function
-//
-// This function prints out a 1 byte value, and then overflows it.
-void overflow();
+// This function Displays data about a student. Notice how the structure we declaired
+//  about can now be used in place of a varaible.
+void ShowStudent(student toShow);
 
 // *****************
 // * Function Code *
@@ -49,80 +60,32 @@ void overflow();
 // Standard startup function.
 int main()
 {
+	// Create a new "student" structure. In this case define the values using the
+	// {} paraters with a list of items.
+	student StudentOne = {"Billy The Kid",20 };
 
-	// Take the text (also called a string) and pass it to cout.
-	// cout is a function which will dump the text to the standard 
-	// output, in this case the screen.
-	cout << "Hello World\n";
-    
-    // Declare two variables. Both are type int. numberBefore is 
-    //  initialized to 5, and numberAfter is initialized to 0.
-    int numberBefore = 5;
-    int numberAfter = 0;
+	// Create a second student. This one will be set after it is created
+	student StudentTwo;
+	// Notice how we can use the '.' notation in order to access items in a structure.
+	StudentTwo.Name = "Silly Singin' Susan";
+	StudentTwo.age = 15;
 
-    // Show the user the value of our two numbers
-    cout << " Before we add, numberBefore is " << numberBefore;
-    cout << " and numberAfter is " << numberAfter << endl;
-    
-    // call our function, passing in the value of numberBefore for both terms.
-    numberAfter = add(numberBefore,numberBefore);
-    
-    // show the user the values of our two numbers
-    cout << " After  we add, numberBefore is " << numberBefore;
-    cout << " and numberAfter is " << numberAfter << endl;
-    
-    // Call our overflow() function to look at what what happens if we
-    //  exceed our variable's range.
-    overflow();
-    
-	// It's good practice to return a 0 from main. In most cases it
-	// doesn't matter, but sometimes other program will look at this to
-	// see if main quit the way it was intended to. 
+	// Show our student data
+	ShowStudent(StudentTwo);
+	ShowStudent(StudentOne);
+
+	// It's good practice to return a 0 from main. 
 	return 0;
 }
 
 //------------------------------------------------------------------------------
-// Implementation of add()
+// Implementation of ShowStudent()
 // 
-// This is where we tell the computer what to do when the add function is called
+// This function knows how to put a student structure out to the display
 //
-// The function header must match our declaration down here, or the code will
-//  not build. The compiler even pays attention to capitalization. so if this was
-//  Add() instead of add() it would give us an error.
-int add(int x, int y)
+// Since the return type is "void", we don't need a return call in this function
+void ShowStudent(student toShow)
 {
-    // A debuging note - this allows us to see when this function is called.
-    cout << " ** We called add(" << x << "," << y << ")! **\n";
-    
-    // take x and y, add them together and return the result
-    return x + y;
+	cout << " Student Name is: " << toShow.Name << ", age:" << toShow.age << " years.\n";
 }
 
-//------------------------------------------------------------------------------
-// Implementation of overflow()
-void overflow()
-{
-    // A debuging note - this allows us to see when this function is called.
-    cout << " ** We called overflow()! **\n";
-    
-    // declar a 1 byte variable, with a value of 100.
-    unsigned char ourLocalChar = 100; // unsigned char can hold from 0 to 255.
-    
-    // Display the current value of ourLocalChar
-    //
-    // The (int) is used to type cast from a char to a int. This is because
-    //  cout wants to show a char as a single character. 
-    //
-    // if you want to see this removed (int) from the line below, and instead of
-    // showing 100 your program output will show a "d". This is because 100 in
-    // ASCII represents a d. you can see a conversion table at 
-    // http://www.asciitable.com/ if your interested...
-    cout << " Our Local Char variable is " << (int)ourLocalChar << endl;
-    
-    // now, the max value of char is 255. What happens if I add 156 to
-    //  ourLocalChar which is at 100?
-    ourLocalChar = ourLocalChar + 157;
-    
-    // Display the current value of ourLocalChar
-    cout << " Our Local Char variable is " << (int)ourLocalChar << endl;
-}
